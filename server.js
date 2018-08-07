1e3ee7dbe938ba42789a532d33ec5d8a047bc2f3
Oliver Nural <onural@and.digital>
2018-08-07 20:07:39 +0100

    added some files for the github scraper bot to test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./app')
 const PORT = 8080
 
-app.listen(PORT, () => {
+app.listen(PORT, (err) => {
+    if (err) 
     console.log(`Express server listening on port ${PORT}!`)
 })
